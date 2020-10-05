0a951eb3b640e0bc69d16dda957a366f65bdfcde
nekobato <nekobato@gmail.com>
2020-10-05 23:08:49 +0900

    ignore and wips

---
+++ stories/Features/AutoFocusNextInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" class="nn-text-field" @input="onInput" :value="value" />
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AutoFocusNextInput",
+  prop: ["next", "maxlength", "value"],
+  methods: {
+    onInput() {
+      this.$emit("input");
+      if (this.value === this.mazlength) {
+        document.querySelector(this.next).focus();
+      }
+    },
+  },
+});
+</script>
+<style scoped></style>
---
+++ stories/Features/PopDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <details>
+    <summary><slot name="summary" /></summary>
+    <slot name="detail" />
+  </details>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PopDetail",
+});
+</script>
+<style scoped></style>
